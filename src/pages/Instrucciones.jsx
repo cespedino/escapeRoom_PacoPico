dbbdb39bbc9023acef51171bef2c0a02767a21b7
Pablo <pncm02@gmail.com>
2024-07-15 16:44:03 +0200

    Página instrucciones y css

--- src/pages/Instrucciones.jsx
+++ src/pages/Instrucciones.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 function Instrucciones() {
   return (
-    <div>
+    <div className='pag-principales'>
       <h1>Instrucciones</h1>
-      <p>Bienvenidos a la yincana. Aquí encontraréis </p>
+      <p>Esto es una pequeña guía de lo que vas a ver a continuación.<br />
+        Tu regalo es una especie de juego de rol en la vida real. <br />
+        Como el juego es en la realidad y la vida es un mundo difícil, el reto lo afrontarás solo. <br />
+        Reglas: </p>
+      <p className='extra-margin'>  
+        1.	No mirar el código del proyecto. <br />
+        2.	El dispositivo desde donde lo empieces será tu centro de operaciones (preferiblemente usa el HP porque es el único donde se ha <br />
+            testeado que funciona, pero si prefieres el tuyo o sabes ponerlo en un móvil lo puedes intentar). <br />
+        3.	No está permitido usar ninguna ayuda externa sin la supervisión de algún organizador (google, chatGPT, maps, WhatsApp, etc.), <br />
+            de hecho, no deberías poder usar el móvil salvo que se te diga lo contrario. <br />
+        4.	Esta permitido salir de la casa y la urbanización. Los límites son la rotonda de colores por el este, la universidad por el sur, <br />
+            por el norte puerto de la torre y al oeste no vayas mucho. <br />
+        5.	Todo lo que se cuenta es figurado, pero para una mejor experiencia se recomienda sumergirse en la narración. Pasando por usar gafas de sol. (bastante opcional) <br />
+      </p>
+      <p>Esperemos que te guste, se ha hecho con mucho cariño y con el fin de que lo disfrutes al máximo.</p>
     </div>
   );
 }
