69fda0a346551c1939d960a85dc28c8d00b386a6
Pablo <pncm02@gmail.com>
2024-07-15 15:51:34 +0200

    Ajuste en el archivo de vite y react-bootstrap

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://cespedino.github.io/escapeRoom_PacoPico/vite-deploy'
+  base: 'https://cespedino.github.io/escapeRoom_PacoPico/vite-deploy',
+  build: {
+    rollupOptions: {
+      // Asegúrate de que Vite resuelva correctamente react-bootstrap
+      external: ['react-bootstrap', 'bootstrap'],
+    },
+  }
 })
